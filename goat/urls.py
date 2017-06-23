f8783d0e9c4f33983e4d80586ccb904e9fd387b1
lukebiggerstaff <lukebiggerstaff@gmail.com>
2017-06-23 10:00:21 -0500

    first passing unit test

--- goat/urls.py
+++ goat/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from lists import views
 
 urlpatterns = [
     url(r'^$', views.home_page,name='home'),
