632c86b28b7f95185bd406e149ad3a99b8e9e1ed
lukebiggerstaff <lukebiggerstaff@gmail.com>
2017-06-23 01:20:29 -0500

    imported unittest and created first test class

--- functional_tests.py
+++ functional_tests.py
@@ -1,9 +1,24 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitor(unittest.TestCase):
 
-assert 'To-Do' in browser.title
+    """Docstring for NewVisitor. """
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
 
 
-browser.quit()
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
+
