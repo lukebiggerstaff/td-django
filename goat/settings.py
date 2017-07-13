29bb3d6aa87fe65072a12acb868766c6da1e658d
lukebiggerstaff <lukebiggerstaff@gmail.com>
2017-07-13 16:42:09 -0500

    spiked in custom passwordless auth backend

--- goat/settings.py
+++ goat/settings.py
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lists',
+    'accounts',
+]
+
+AUTH_USER_MODEL = 'accounts.ListUser'
+AUTHENTICATION_BACKENDS = [
+    'accounts.authentication.PasswordlessAuthenticationBackend',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +126,28 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
+
+# gmail account information
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'goattesting@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+
+# Enable verbose logging to terminal
+LOGGING = {
+    'version' : 1,
+    'disable_existing_loggers' : False,
+    'handlers' : {
+        'console' : {
+            'leve' : 'DEBUG',
+            'class' : 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django' : {
+            'handlers' : ['console'],
+        },
+    },
+    'root' : {'level' : 'INFO'},
+}
