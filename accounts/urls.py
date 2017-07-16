7150ea52a48329f725cc34b4933fa1678ebb2603
lukebiggerstaff <lukebiggerstaff@gmail.com>
2017-07-16 06:35:51 -0500

    updated logout button to use built in logout django view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from accounts import views as accounts_views
+from django.contrib.auth.views import logout
 
 
 urlpatterns = [
     url(r'^send_login_email$', accounts_views.send_login_email,name='send_login_email'),
     url(r'^login$', accounts_views.login,name='login'),
+    url(r'^logout$', logout, {'next_page': '/'}, name='logout'),
 ]
